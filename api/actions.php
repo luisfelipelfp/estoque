a8a16677f01cbdcee0c2203fd3c6616a175ce78e
luisfelipelfp <luis.lfp.felipe@gmail.com>
2025-08-19 13:53:43 -0300

    senha

--- api/actions.php
+++ api/actions.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "#Shakka01";
 $dbname = "estoque";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
