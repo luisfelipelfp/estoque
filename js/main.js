c92ffb6cfca4308d7d6d7bb18a28e64434a8330c
luisfelipelfp <luis.lfp.felipe@gmail.com>
2025-09-02 10:18:14 -0300

    js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 window.onload = () => {
+    // Continua carregando os produtos logo ao abrir
     listarProdutos();
-    listarMovimentacoes();
+
+    // Não chama listarMovimentacoes() na inicialização
+    // Agora as movimentações só aparecem após o usuário aplicar os filtros
 };
