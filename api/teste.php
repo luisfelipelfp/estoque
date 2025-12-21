1a10825b1c946e04298b6d60e233b2c4ce563ad4
luisfelipelfp <luis.lfp.felipe@gmail.com>
2025-12-21 16:26:22 -0300

    teste

--- api/teste.php
+++ api/teste.php
@@ -1,15 +1,4 @@
 <?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/log.php';
 require_once __DIR__ . '/utils.php';
-require_once __DIR__ . '/db.php';
 
-initLog('teste');
-
-try {
-    $conn = db();
-    json_response(true, 'Conexão OK');
-} catch (Throwable $e) {
-    json_response(false, $e->getMessage());
-}
+json_response(true, 'Teste OK');
