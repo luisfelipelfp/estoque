8904307ab5e1181eab2f06337dd7fcddfeb8406f
luisfelipelfp <luis.lfp.felipe@gmail.com>
2026-03-04 21:07:48 -0300

    novo

--- api/test_session.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "session_loaded=" . (extension_loaded("session") ? "yes" : "no");
-echo "\nsession_save_path=" . ini_get("session.save_path");
-echo "\n";
--- api/teste.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/utils.php';
-
-json_response(true, 'Teste OK');
